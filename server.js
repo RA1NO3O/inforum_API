4f6c0e23da4d133a8418536de7301a3ff9f94a12
RA1NO3O <raincaller@outlook.com>
2021-03-05 23:11:06 +0800

    updated contentType

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.all('*', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin','*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-        res.setHeader("Content-Type", "application/json;charset=utf-8");
+        // res.setHeader("Content-Type", "application/json;charset=utf-8");
   if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
   else  next();
 });
