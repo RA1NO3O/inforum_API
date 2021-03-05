2e8323b48bc501c30796a370f9007c13a1ab0ba4
RA1NO3O <raincaller@outlook.com>
2021-03-05 23:04:43 +0800

    updated server

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ app.listen(port, () => {
   console.clear();
   console.log('app listening on http://localhost:' + port);
 });
+
 const bodyParser = require('body-parser');
+
+app.all('*', function(req, res, next) {
+  console.log(req.headers.origin)
+  console.log(req.environ)
+  res.setHeader('Access-Control-Allow-Origin','*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+        res.setHeader("Content-Type", "application/json;charset=utf-8");
+  if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+  else  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(index);
