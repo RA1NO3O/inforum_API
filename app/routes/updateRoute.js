1170dbe31b3e8b194e87a506d74842d41cbc85d1
RA1NO3O <raincaller@outlook.com>
2021-03-06 13:41:38 +0800

    fixed updateRoute

--- app/routes/updateRoute.js
+++ app/routes/updateRoute.js
@@ -1,32 +1,14 @@
 const express = require("express");
 var server = express.Router();
-var sqlUpdate = require('../data/sqlInsert');
+var sqlUpdate = require('../data/sqlUpdate');
 const sql = require('mssql');
 var myDate;
 
 //编辑帖子
-server.post('/api/editPost/', function (req, res) {
-    sql.connect(config).then(function () {
-        new sql.Request()
-            .input('postID', sql.Int, req.body.postID)
-            .input('title', sql.NVarChar, req.body.title == 'null' ? null : req.body.title)
-            .input('content', sql.NVarChar, req.body.content)
-            .input('tags', sql.NVarChar, req.body.tags == 'null' ? null : req.body.tags)
-            .input('imgURL', sql.VarChar, req.body.imgURL == 'null' ? null : req.body.imgURL)
-            .query(
-                `UPDATE tbPost
-                     SET title=@title, body=@content, tags=@tags, imageURL=@imgURL, lastEditTime=getDate()
-                     WHERE postID = @postID;`
-            ).then(function (recordset) {
-                res.send('success.');
-                console.dir(recordset); //在终端输出
-                myDate = new Date();
-                console.log('post ' + req.body.postID + 'has been updated at ' + myDate.toLocaleTimeString());
-            });
-    }).catch(function (err) {
-        console.log(err);
-        res.status(500).send(err);
-    });
+server.post('/api/editPost/', async (req, res) => {
+    let r = await sqlUpdate.editPost(req);
+    console.dir(r);
+    res.send(r != null ? 'success' : 'error.');
 });
 
 //点赞👍
@@ -42,7 +24,7 @@ server.post('/api/thumbDown/', async (req, res) => {
     res.send(r != null ? 'success' : 'error.');
 });
 //收藏⭐
-server.post('/api/starPost/', async (req, res) =>{
+server.post('/api/starPost/', async (req, res) => {
     let r = await sqlUpdate.starPost(req);
     console.dir(r);
     res.send(r != null ? 'success' : 'error.');
