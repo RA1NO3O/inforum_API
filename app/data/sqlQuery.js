838890a4376a20d48ee680aaf38c9c69f1144b2e
RA1NO3O <raincaller@outlook.com>
2021-03-07 20:50:39 +0800

    Update sqlQuery.js

--- app/data/sqlQuery.js
+++ app/data/sqlQuery.js
@@ -162,7 +162,7 @@ module.exports = {
                             ,IIF([user_ID]=@currentUserID,[like_State],null)AS like_State
                             ,IIF([user_ID]=@currentUserID,[collectTime],null)AS collectTime
                             FROM [Inforum_Data_Center].[dbo].[getPosts]
-                            WHERE (user_ID=@currentUserID OR user_ID IS NULL) AND editorID=@userID
+                            WHERE editorID=@userID
                             ORDER BY lastEditTime DESC;`
                     ).then((recordset) => {
                         back(recordset);
